059751eb75ea161e411cf0b37c4d71cbf7246f0f
Kirim Lee <uthi1004@gmail.com>
2020-07-10 09:40:57 +0900

    유저어드민 모델 보완

--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,11 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
+THIRD_PARTY_APPS = []
+
 PROJECT_APPS = ["users.apps.UsersConfig"]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 
 MIDDLEWARE = [
