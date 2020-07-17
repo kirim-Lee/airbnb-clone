e048150392e588955ee4963dfcb5102b0628fab6
Kirim Lee <uthi1004@gmail.com>
2020-07-17 18:28:55 +0900

    다대다 관계의 룸 속성 모델 추가

--- rooms/models.py
+++ rooms/models.py
@@ -3,7 +3,47 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ AbstractItem """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    """ RoomType Model Definition """
+
+    pass
+
+
+class Amenity(AbstractItem):
+
+    """ Amenity Model Definition """
+
+    pass
+
+
+class Facility(AbstractItem):
+
+    """ Facility Model Definition """
+
+    pass
+
+
+class HouseRule(AbstractItem):
+
+    """ HouseRule Model Definition """
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
@@ -22,4 +62,11 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenity = models.ManyToManyField(Amenity, blank=True)
+    facility = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
 
