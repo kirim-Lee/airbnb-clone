bf4864d5357d99d873c8fd73d414f47f0978d10e
Kirim Lee <uthi1004@gmail.com>
2020-07-17 18:48:35 +0900

    Room photo model 추가

--- rooms/admin.py
+++ rooms/admin.py
@@ -9,5 +9,14 @@ class RoomAdmin(admin.ModelAdmin):
 
 @admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+
+    """ Item Admin Definition """
+
     pass
 
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    """ Photo Admin Definition"""
+    pass
