f1246cf665740d8a33fdd9f9831c2eb1033ace1d
Kirim Lee <uthi1004@gmail.com>
2020-07-29 16:05:08 +0900

    Reservation, List model definition

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ PROJECT_APPS = [
     "users.apps.UsersConfig",
     "rooms.apps.RoomsConfig",
     "reviews.apps.ReviewsConfig",
+    "reservations.apps.ReservationsConfig",
+    "lists.apps.ListsConfig",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
