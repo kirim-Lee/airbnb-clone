23a39dcf4469d53433c321a51f53f8780860528c
Kirim Lee <uthi1004@gmail.com>
2020-08-20 11:31:46 +0900

    paginator exception error controll

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 page_size = 10
 
@@ -8,7 +8,11 @@ page_size = 10
 def all_rooms(request):
     page = request.GET.get("page", default="1")
     room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, page_size)
-    rooms = paginator.get_page(page)
+    paginator = Paginator(room_list, page_size, orphans=3, allow_empty_first_page=True)
 
-    return render(request, "rooms/home.html", {"rooms": rooms})
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"rooms": rooms})
+
+    except EmptyPage:
+        return redirect("/")
